614ba00950c2bdc7f5ff5871948af961c6cc4fe8
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2024-10-04 12:11:11 +0800

    Create DNA as a variable.pl

---
+++ DNA as a variable.pl
@@ -0,0 +1,7 @@
+#!/usr/local/bin/perl
+# Storing DNA in a variable, and printing it out
+# First we store the DNA in a variable called $DNA
+$DNA = 'ACGGGAGGACGGGAAAATTACTACGGCATTAGC';
+# Next, we print the DNA onto the screen
+print "$DNA\n";
+print "\n";
